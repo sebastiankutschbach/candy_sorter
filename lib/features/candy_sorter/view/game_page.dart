9f19f229a170926aa236020897a055dd4b6fcd47
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2022-03-17 19:24:32 +0100

    shared prefs are working

--- lib/features/candy_sorter/view/game_page.dart
+++ lib/features/candy_sorter/view/game_page.dart
@@ -5,6 +5,7 @@ import 'package:candy_sorter/features/candy_sorter/view/bowl_area.dart';
 import 'package:candy_sorter/features/candy_sorter/view/candy_area.dart';
 import 'package:candy_sorter/features/candy_sorter/view/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
@@ -17,13 +18,16 @@ class _GamePageState extends State<GamePage> {
   late Game game;
   late Timer _timer;
 
+  SharedPreferences? prefs;
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+
     _createGame();
   }
 
-  _createGame() {
+  void _createGame() {
     game = Game(
       colors: [
         Colors.red,
@@ -31,8 +35,8 @@ class _GamePageState extends State<GamePage> {
         Colors.blueGrey,
         Colors.cyan,
         Colors.orange,
-      ],
-      numberOfCandies: 100,
+      ].sublist(0, prefs?.getInt('noOfColors') ?? 5),
+      numberOfCandies: prefs?.getInt('noOfCandies') ?? 100,
       gameArea: Size(
         MediaQuery.of(context).size.width,
         MediaQuery.of(context).size.height / 2,
@@ -53,53 +57,69 @@ class _GamePageState extends State<GamePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: _createGame,
-                child: const Text('New Game'),
-              ),
-              ElevatedButton(
-                  onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => SettingsPage(),
-                        ),
-                      ),
-                  child: Text("Settings"))
-            ],
-          ),
-          Text(
-            "Time elapsed: ${game.stopwatch.elapsed.toString().split('.')[0]}",
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                'Candies left: ${game.candies.length}',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Text(
-                'Candies sorted: ${game.numberOfCandies - game.candies.length}',
-                style: Theme.of(context).textTheme.headline6,
-              )
-            ],
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 2,
-            child: CandyArea(
-              game: game,
-            ),
-          ),
-          Expanded(
-            child: BowlArea(game: game, onRemoveCandy: _onRemoveCandy),
-          ),
-        ],
-      ),
+    return FutureBuilder(
+      future: _initPrefs(),
+      builder: (context, snapshot) =>
+          snapshot.hasData ? _loadedScaffold() : _loadingScaffold(),
     );
+  }
+
+  Widget _loadedScaffold() => Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _createGame,
+                  child: const Text('New Game'),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ),
+                  ),
+                  child: const Text("Settings"),
+                )
+              ],
+            ),
+            Text(
+              "Time elapsed: ${game.stopwatch.elapsed.toString().split('.')[0]}",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Candies left: ${game.candies.length}',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text(
+                  'Candies sorted: ${game.numberOfCandies - game.candies.length}',
+                  style: Theme.of(context).textTheme.headline6,
+                )
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: CandyArea(
+                game: game,
+              ),
+            ),
+            Expanded(
+              child: BowlArea(game: game, onRemoveCandy: _onRemoveCandy),
+            ),
+          ],
+        ),
+      );
+
+  Widget _loadingScaffold() => const Scaffold(
+        body: CircularProgressIndicator(),
+      );
+
+  _initPrefs() async {
+    prefs ??= await SharedPreferences.getInstance();
+    return prefs;
   }
 }
--- lib/features/candy_sorter/view/settings_page.dart
+++ lib/features/candy_sorter/view/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool prefsLoaded = false;
   int _noOfCandies = 100;
   int _noOfColors = 5;
 
@@ -15,41 +17,56 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setInt('noOfCandies', _noOfCandies);
+        await prefs.setInt('noOfColors', _noOfColors);
         return true;
       },
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text('Number of candies: $_noOfCandies'),
-              Slider(
-                min: 1,
-                max: 100,
-                divisions: 99,
-                value: _noOfCandies.toDouble(),
-                onChanged: (value) => setState(
-                  () => _noOfCandies = value.toInt(),
+      child: FutureBuilder(
+        future: _loadPrefs(),
+        builder: (context, data) => Scaffold(
+          appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text('Number of candies: $_noOfCandies'),
+                Slider(
+                  min: 1,
+                  max: 100,
+                  divisions: 99,
+                  value: _noOfCandies.toDouble(),
+                  onChanged: (value) => setState(
+                    () => _noOfCandies = value.toInt(),
+                  ),
                 ),
-              ),
-              Text('Number of colors: $_noOfColors'),
-              Slider(
-                min: 1,
-                max: 5,
-                divisions: 4,
-                value: _noOfColors.toDouble(),
-                onChanged: (value) => setState(
-                  () => _noOfColors = value.toInt(),
+                Text('Number of colors: $_noOfColors'),
+                Slider(
+                  min: 1,
+                  max: 5,
+                  divisions: 4,
+                  value: _noOfColors.toDouble(),
+                  onChanged: (value) => setState(
+                    () => _noOfColors = value.toInt(),
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: const Text('Back to game')),
-            ],
+                ElevatedButton(
+                    onPressed: () => Navigator.of(context).maybePop(),
+                    child: const Text('Back to game')),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  _loadPrefs() async {
+    if (!prefsLoaded) {
+      final prefs = await SharedPreferences.getInstance();
+      _noOfColors = prefs.getInt('noOfColors') ?? 5;
+      _noOfCandies = prefs.getInt('noOfCandies') ?? 100;
+    }
+    prefsLoaded = true;
   }
 }
